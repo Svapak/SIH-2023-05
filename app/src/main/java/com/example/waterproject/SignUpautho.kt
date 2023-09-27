92c1099e3d03e6f0ac26abd5ff4fd354a001fa38
Aditya kumar <adityakumarmon123@gmail.com>
2023-09-28 05:21:30 +0530

    9th commit

--- app/src/main/java/com/example/waterproject/SignUpautho.kt
+++ app/src/main/java/com/example/waterproject/SignUpautho.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -23,9 +26,31 @@ class SignUpautho : AppCompatActivity() {
     private lateinit var dialogBox:Dialog
     private lateinit var mfirestore: FirebaseFirestore
     private lateinit var mdatabaseref: DatabaseReference
+   private lateinit var type: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_upautho)
+        val items = listOf(
+            "none",
+            "Urban Flooding",
+            "Rural Flooding",
+            "Oil Spill",
+            "Tsunami",
+            "Polluted River",
+            "Drought",
+            "Drainage problems"
+        )
+        type=""
+        val autoComplete: AutoCompleteTextView = findViewById(R.id.typeEntryField)
+        val adapter = ArrayAdapter(this, R.layout.item_list, items)
+        autoComplete.setAdapter(adapter)
+        autoComplete.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val itemSelected = parent.getItemAtPosition(position)
+                type = itemSelected.toString()
+            }
+
+
         val btnauth=findViewById<RadioButton>(R.id.rbAuthority)
         val btnresident=findViewById<RadioButton>(R.id.rbResident)
         btnresident.setOnClickListener {
@@ -56,7 +81,7 @@ class SignUpautho : AppCompatActivity() {
         val email:String=findViewById<EditText>(R.id.EmailEntryField).text.toString().trim{it<=' '}
         val password:String=findViewById<EditText>(R.id.passwordEntryField).text.toString().trim{it<=' '}
         val code:String=findViewById<EditText>(R.id.codeEntryField).text.toString().trim(){it<=' '}
-        if(validateForm(name,email,password,code))
+        if(validateForm(name,email,password,code,type))
         {
             showProgressDialog("Signing Up...")
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password
@@ -75,7 +100,7 @@ class SignUpautho : AppCompatActivity() {
                     toast.duration.toLong()
                     toast.show()
 
-                    var user= users(name = name, email = email, code = code, ischecked = "org")
+                    var user= users(name = name, email = email, code = code, ischecked = "org", type = type)
                     mdatabaseref.child(firebaseuser.uid).setValue(user).addOnCompleteListener {
 
                         finishAffinity()
@@ -110,7 +135,7 @@ class SignUpautho : AppCompatActivity() {
 
 
     }
-    private fun validateForm(name:String,email:String,password:String,code:String):Boolean
+    private fun validateForm(name:String,email:String,password:String,code:String,type:String):Boolean
     {
         return when{
             TextUtils.isEmpty(name)->
@@ -131,6 +156,11 @@ class SignUpautho : AppCompatActivity() {
             TextUtils.isEmpty(code)->
             {
                 errorSnackBar("Please Enter your PinCode")
+                false
+            }
+            TextUtils.isEmpty(type) ->
+            {
+                errorSnackBar("Please enter the type")
                 false
             }
             else->
