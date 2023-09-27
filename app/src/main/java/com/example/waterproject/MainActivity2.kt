d35852d7635fda0b731239fd9f9d6daa41d9fea6
aryan <aryanrdat@gmail.com>
2023-09-27 19:39:45 +0530

    nav commit

--- app/src/main/java/com/example/waterproject/MainActivity2.kt
+++ app/src/main/java/com/example/waterproject/MainActivity2.kt
@@ -2,10 +2,14 @@ package com.example.waterproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var navBar: ChipNavigationBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        navBar=findViewById(R.id.navBar)
+        navBar.setMenuResource(R.menu.navmenu)
     }
 }
